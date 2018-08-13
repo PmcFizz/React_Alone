b830c939609304adab63e60f3214ce04cb819c30
pangmengchen <mengchen_0212@foxmail.com>
2018-08-13 20:24:46 +0800

    解决困扰我很久的页面不能正常显示数据的问题

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -24,7 +24,7 @@ const Index = ({
   const listProps = {
     pagination,
     dataSource: list,
-    loading: loading.effects['post/query'],
+    loading: loading.effects['contact/query'],
     onChange (page) {
       dispatch(routerRedux.push({
         pathname,
